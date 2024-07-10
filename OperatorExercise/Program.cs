8d0075650a2fda939f6ec3fec1e5454dc428d571
Alex Leifermann <aleif92@outlook.com>
2024-07-10 13:55:06 -0500

    OperatorExercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,27 @@
 {
     public class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            //Exercise 1
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} with a remainder of {remainder}.");
+            Console.WriteLine("what is the radius?");
+            var radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"This is the area of your circle:" + AreaOfCircle(radius));
+            
+        }
+        //defining area of circle method
+        public static double  AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
         }
     }
 }
